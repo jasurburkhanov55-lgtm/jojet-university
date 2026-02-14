850fb39cde621fda508daa53cf6faf36f47b301f
jasurburkhanov55-lgtm <jasurburkhanov55@gmail.com>
2026-02-14 15:39:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jojet-university/',
+  base: 'https://jasurburkhanov55-lgtm.github.io/jojet-university/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
